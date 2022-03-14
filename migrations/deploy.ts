a96122869f0927cbcc6f061ea1f1d32cf54105df
badconfig <array.clean@gmail.com>
2022-03-14 18:53:43 +0300

    fix

--- migrations/deploy.ts
+++ migrations/deploy.ts
@@ -2,11 +2,20 @@
 // single deploy script that's invoked from the CLI, injecting a provider
 // configured from the workspace's Anchor.toml.
 
+import {Provider} from "@project-serum/anchor";
+
 const anchor = require("@project-serum/anchor");
 
-module.exports = async function (provider) {
   // Configure client to use the provider.
-  anchor.setProvider(provider);
+  anchor.setProvider(anchor.Provider.env());
+  //// Read the generated IDL.
+    const idl = JSON.parse(
+        require("fs").readFileSync("./target/idl/nft_staking.json", "utf8")
+    );
 
-  // Add your deploy script here.
-}
+    //Address of the deployed program
+    const programId = new anchor.web3.PublicKey("GBpQdoXPzopEfP6Nmwv6PprWUo1nKcziK6Bqjeb7pCzi");
+
+    //Generate the program client from IDL
+    const program = new anchor.Program(idl, programId);
+    console.log(program)
